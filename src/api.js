0d874f3f4aeb14d4951dfea3e4b832d2e45b8769
Gabrielcesar9 <gabriel.cesar.pereira.9@gmail.com>
2020-02-02 13:27:22 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const serverless = require('serverless-http')
+const express = require("express");
+const serverless = require("serverless-http")
 
 const app = express();
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({'hello':'hi!'});
+router.get("/", (req, res) => {
+    res.json({
+        "hello":"hi!"
+    });
 });
 app.use('/.netlify/functions/api', router);
-module.exports(app);
+module.exports = app;
 module.exports.handler = serverless(app);
